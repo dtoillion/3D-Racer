1f2c6aa31b42621e7b530b6fc31b17bdb7b3c099
dtoillion <toillion@gmail.com>
2016-09-30 10:05:33 -0700

    waypoint AI, 9 cars, camera switcher, car switcher

---
+++ 3D-Racer/Assets/_Scripts/CameraSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitcher : MonoBehaviour
+{
+
+  [SerializeField] private GameObject[] cameras = new GameObject[4];
+
+	void Update ()
+  {
+    if (Input.GetKeyDown("1") && cameras[0] != null)
+    {
+      switchCameras(0);
+    }
+    else if (Input.GetKeyDown("2") && cameras[1] != null)
+    {
+      switchCameras(1);
+    }
+    else if (Input.GetKeyDown("3") && cameras[2] != null)
+    {
+      switchCameras(2);
+    }
+    else if (Input.GetKeyDown("4") && cameras[3] != null)
+    {
+      switchCameras(3);
+    }
+	}
+
+  private void switchCameras(int keyNum)
+  {
+    for (int i = 0; i < cameras.Length; i++)
+    {
+      if (cameras[i] != null && keyNum != i) {
+        // turn camera off
+        cameras[i].SetActive(false);
+      } else {
+        // turn camera on
+        cameras[i].SetActive(true);
+      }
+    }
+  }
+}
