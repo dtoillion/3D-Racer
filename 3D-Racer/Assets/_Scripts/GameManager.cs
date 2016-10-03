b80ade9790270c9dafd1e386c72623ce598c8406
dtoillion <toillion@gmail.com>
2016-10-03 14:09:28 -0700

    Main menu with scene manager, UI, level 1 and 8 placeholders

---
+++ 3D-Racer/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+  public bool muted = false;
+  public AudioClip[] musicbg;
+  AudioSource backgroundMusic;
+
+  private int i;
+
+
+	void Awake ()
+  {
+    DontDestroyOnLoad(gameObject);
+    backgroundMusic = GetComponent <AudioSource> ();
+	}
+
+  void Start ()
+  {
+    i = UnityEngine.Random.Range(0,musicbg.Length);
+    StartCoroutine("Playlist");
+  }
+
+  public void MuteMusic ()
+  {
+    if(!muted)
+    {
+      muted = true;
+      backgroundMusic.Pause();
+    } else {
+      muted = false;
+    }
+	}
+
+  IEnumerator Playlist()
+  {
+    while(true)
+    {
+      yield return new WaitForSeconds(1.0f);
+      if((!backgroundMusic.isPlaying) && (!muted))
+      {
+        if(i != (musicbg.Length -1)) {
+          i++;
+          backgroundMusic.clip = musicbg[i];
+          backgroundMusic.Play();
+        } else {
+          i=0;
+          backgroundMusic.clip= musicbg[i];
+          backgroundMusic.Play();
+        }
+      }
+    }
+  }
+
+}
---
+++ 3D-Racer/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+  public GameObject loadingImage;
+
+  public void LoadScene(int level)
+  {
+    loadingImage.SetActive(true);
+    SceneManager.LoadScene(level);
+  }
+
+  public void ExitGame()
+  {
+    Application.Quit();
+  }
+}
