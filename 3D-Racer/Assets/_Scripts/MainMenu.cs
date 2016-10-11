6e9db8c40a436741824f1975e7693ef434d26e4c
dtoillion <toillion@gmail.com>
2016-10-10 22:33:32 -0700

    lighting and level design for track 1 through 4

--- 3D-Racer/Assets/_Scripts/MainMenu.cs
+++ 3D-Racer/Assets/_Scripts/MainMenu.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
@@ -6,6 +7,7 @@ public class MainMenu : MonoBehaviour
 {
   public GameObject loadingImage;
   public GameObject[] Trophies;
+  public Button[] LevelButtons;
 
   void Start ()
   {
@@ -14,15 +16,51 @@ public class MainMenu : MonoBehaviour
 
   public void SetupTrophies ()
   {
-    if(GameManager.control.Trophy01){ Trophies[0].SetActive(true); }
-    if(GameManager.control.Trophy02){ Trophies[1].SetActive(true); }
-    if(GameManager.control.Trophy03){ Trophies[2].SetActive(true); }
-    if(GameManager.control.Trophy04){ Trophies[3].SetActive(true); }
-    if(GameManager.control.Trophy05){ Trophies[4].SetActive(true); }
-    if(GameManager.control.Trophy06){ Trophies[5].SetActive(true); }
-    if(GameManager.control.Trophy07){ Trophies[6].SetActive(true); }
-    if(GameManager.control.Trophy08){ Trophies[7].SetActive(true); }
-    if(GameManager.control.Trophy09){ Trophies[8].SetActive(true); }
+    if(GameManager.control.Trophy01)
+    {
+      Trophies[0].SetActive(true);
+      LevelButtons[1].interactable = true;
+    }
+    if(GameManager.control.Trophy02)
+    {
+      Trophies[1].SetActive(true);
+      LevelButtons[2].interactable = true;
+    }
+    if(GameManager.control.Trophy03)
+    {
+      Trophies[2].SetActive(true);
+      LevelButtons[3].interactable = true;
+    }
+    if(GameManager.control.Trophy04)
+    {
+      Trophies[3].SetActive(true);
+      LevelButtons[4].interactable = true;
+    }
+    if(GameManager.control.Trophy05)
+    {
+      Trophies[4].SetActive(true);
+      LevelButtons[5].interactable = true;
+    }
+    if(GameManager.control.Trophy06)
+    {
+      Trophies[5].SetActive(true);
+      LevelButtons[6].interactable = true;
+    }
+    if(GameManager.control.Trophy07)
+    {
+      Trophies[6].SetActive(true);
+      LevelButtons[7].interactable = true;
+    }
+    if(GameManager.control.Trophy08)
+    {
+      Trophies[7].SetActive(true);
+      LevelButtons[8].interactable = true;
+    }
+    if(GameManager.control.Trophy09)
+    {
+      Trophies[8].SetActive(true);
+      LevelButtons[9].interactable = true;
+    }
   }
 
   public void LoadScene(int level)
---
+++ 3D-Racer/Assets/_Scripts/PlaySoundOnHover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnHover : MonoBehaviour
+{
+  public AudioSource hoverAudio;
+
+  void Awake ()
+  {
+    hoverAudio = GetComponent <AudioSource> ();
+  }
+
+	void OnMouseEnter()
+  {
+    Debug.Log("moused over");
+    if(!hoverAudio.isPlaying)
+      hoverAudio.Play();
+	}
+
+}
